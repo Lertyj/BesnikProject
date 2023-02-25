fffdd0d8b61d2e01ccfc1f0e4c9ca3e7045a49e7
Lertyj <alexroleplay2019@gmail.com>
2023-02-25 07:32:16 +0300

    адаптированное меню, адаптивные блоки при картинке, адаптирована часть сайта

---
+++ script.js
@@ -0,0 +1,8 @@
+menu.onclick = function meFunction(){
+    var x = document.getElementById('mytopnav')
+    if (x.className === "topnav") {
+        x.className += " responsive"
+    } else {
+        x.className = "topnav"
+    }
+}
